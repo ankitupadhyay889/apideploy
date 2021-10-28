677951268dc63344f65fa4f7b447e60716cd0fdb
Ankit <ankitupa7799@gmail.com>
2021-10-28 11:11:26 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import {Table} from "react-bootstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ApiCall = () => {
 
-export default App;
+    const [data, setdata] = useState([]);
+
+    const loadData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const acct = await res.json();
+      setdata(acct);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, [])
+
+    return (
+        <div className="cs mt-5">
+            <h1> Data Table API </h1>
+            <Table striped bordered hover className="mt-5">
+                <thead>
+                    <tr>
+                    <th>S.No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Username</th>
+                    <th>Website</th>
+                    <th>Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item,id) => {
+                            return(
+                                <tr key={id}>
+                                    <td> {item.id} </td>
+                                    <td> {item.name} </td>
+                                    <td> {item.email} </td>
+                                    <td> {item.username} </td>
+                                    <td> {item.website} </td>
+                                    <td> {item.phone} </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
+        </div>
+    );
+};
+
+export default ApiCall;
